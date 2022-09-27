05014acddec53c62f78c1188eeb06c83948e59c5
GuMuniz <gustavo.munizr13@gmail.com>
2022-09-27 13:24:32 -0300

    Criando o projeto base

---
+++ src/main/java/br/com/clinicsul/software/ClinicSIulApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinicsul.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSIulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSIulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clinicsul/software/ClinicSIulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicsul.software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSIulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
